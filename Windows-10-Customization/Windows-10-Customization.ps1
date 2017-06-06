81e6544985ddc73327aea5e84c891d155cf796d7
Guacamolean <mummyandachainsaw@gmail.com>
2017-06-06 14:58:15 -0400

    Create Windows-10-Customization.ps1

---
+++ Windows-10-Customization/Windows-10-Customization.ps1
@@ -0,0 +1,21 @@
+Function Set-RegistryValues($path,$name)
+{
+    IF (Get-ItemProperty -Path $path -Name $name -ErrorAction SilentlyContinue){
+        Set-ItemProperty -Path $path -Name $name -Value 0
+    }
+    ELSE {
+        New-ItemProperty -Path $path -Name $name -Value 0 -Property DWORD
+    }
+}
+
+$desktopPath = "HKLM:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel"
+$myComputer = "{20D04FE0-3AEA-1069-A2D8-08002B30309D}"
+$userFiles = "{59031a47-3f72-44a7-89c5-5595fe6b30ee}"
+$network = "{F02C1A0D-BE21-4350-88B0-7367FC96EF3C}"
+$trayPath = "HKLM:\Software\Microsoft\Windows\CurrentVersion\Explorer"
+$trayKey = "EnableAutoTray"
+
+Set-RegistryValues -path $desktopPath -name $myComputer
+Set-RegistryValues -path $desktopPath -name $userFiles
+Set-RegistryValues -path $desktopPath -name $network
+Set-RegistryValues -path $trayPath -name $trayKey
