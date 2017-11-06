3a5b5e249ddf8931905acb5aeaacd2bea0575970
Avila <Nery.Avila@mindshift.com>
2017-11-06 15:53:45 -0500

    General cleanup

--- End-User-QA/End-User-QA.ps1
+++ End-User-QA/End-User-QA.ps1
@@ -11,15 +11,11 @@ $bios = Get-WmiObject -Class Win32_Bios
 
 function Write-Log {
     Param(
-        [Parameter(
-            Mandatory=$True
-        )]
+        [Parameter(Mandatory=$True)]
         [String]
         $String,
 
-        [Parameter(
-            Mandatory=$True
-        )]
+        [Parameter(Mandatory=$True)]
         [String]
         $Color
     )
@@ -27,58 +23,55 @@ function Write-Log {
     Write-Host $String -ForegroundColor $Color
 }
 
-function Get-WindowsStatus 
-{
+function Get-WindowsStatus {
     $osVersion = Get-WmiObject Win32_OperatingSystem | Select-Object Caption
     $windowsStatus = Get-WmiObject SoftwareLicensingProduct -Filter "ApplicationID = '55c92734-d682-4d71-983e-d6ec3f16059f'" | Where-Object {$_.LicenseStatus -eq "1" -or $_.LicenseStatus -eq "2"}
-    Switch ($windowsStatus.LicenseStatus){
-	    "1" {Write-Log "$($osVersion.Caption) : Activated." "Green"}
-	    "2" {Write-Log "$($osVersion.Caption) : Not activated." "Red"}
+    Switch ($windowsStatus.LicenseStatus) {
+	    "1" {Write-Log "$($osVersion.Caption) : Activated." -Color "Green"}
+	    "2" {Write-Log "$($osVersion.Caption) : Not activated." -Color "Red"}
     }
 }
 
-function Get-OfficeStatus 
-{
-    IF (Test-Path 'C:\Program Files (x86)\Microsoft Office\Office14\ospp.vbs'){
+function Get-OfficeStatus {
+    if (Test-Path 'C:\Program Files (x86)\Microsoft Office\Office14\ospp.vbs') {
         $officePath = "C:\Program Files (x86)\Microsoft Office\Office14"
         $officeVersion = "Office 2010"
         } 
-        ELSEIF (Test-Path 'C:\Program Files (x86)\Microsoft Office\Office15\ospp.vbs'){
+        elseif (Test-Path 'C:\Program Files (x86)\Microsoft Office\Office15\ospp.vbs') {
             $officePath = "C:\Program Files (x86)\Microsoft Office\Office15"
             $officeVersion = "Office 2013"
         }
-        ELSEIF (Test-Path 'C:\Program Files (x86)\Microsoft Office\Office16\ospp.vbs'){
+        elseif (Test-Path 'C:\Program Files (x86)\Microsoft Office\Office16\ospp.vbs') {
             $officePath = "C:\Program Files (x86)\Microsoft Office\Office16"
             $officeVersion = "Office 2016"
         }
-        ELSEIF (Test-Path 'C:\Program Files\Microsoft Office\Office16\ospp.vbs'){
+        elseif (Test-Path 'C:\Program Files\Microsoft Office\Office16\ospp.vbs') {
             $officePath = "C:\Program Files\Microsoft Office\Office16"
             $officeVersion = "Office 2016"
         }
-        ELSE {
+        else {
         $officePath = $Null
     }
-    IF ($officePath){
+    if ($officePath) {
         $officeStatus = cscript $officePath\ospp.vbs /dstatus | Select-String -Pattern "License Status" -SimpleMatch
-        IF ($officeStatus -like "*LICENSED*"){
-            Write-Log "$officeVersion : Activated." "Green"
+        if ($officeStatus -like "*LICENSED*") {
+            Write-Log "$officeVersion : Activated." -Color "Green"
         }
-        ELSE {
-            Write-Log "$officeVersion : Not activated." "Yellow"
+        else {
+            Write-Log "$officeVersion : Not activated." -Color "Yellow"
         }
     }
-    ELSE {
-        Write-Log "Office : Not installed." "Red"
+    else {
+        Write-Log "Office : Not installed." -Color "Red"
     }
 }
 
-function Get-Kaseya 
-{
+function Get-Kaseya {
     $kaseya = Get-Service -DisplayName "Kaseya*"
     $kaseyaDirectory = Get-ChildItem 'C:\Program Files (x86)' -recurse -include kaseyad.ini -ErrorAction SilentlyContinue | Select-Object DirectoryName
-    IF ($kaseya){
+    if ($kaseya) {
         $kaseyaObject = @(
-            ForEach ($directory in $kaseyaDirectory){
+            foreach ($directory in $kaseyaDirectory) {
             $kaseyaSettings = Get-Content -Path "$($directory.DirectoryName)\kaseyad.ini"
             $kaseyaGroup = $kaseyaSettings | Select-String -Pattern "User_Name"
             $kaseyaGroup = $kaseyaGroup -Split "\s+" | Select-Object -Last 1 
@@ -89,68 +82,64 @@ function Get-Kaseya
                 }	
             }
             )
-    ForEach ($install in $kaseyaObject){
-        Write-Log "Kaseya checking into $($install.Group) on $($install.Server)." "Green"
+    foreach ($install in $kaseyaObject) {
+        Write-Log "Kaseya checking into $($install.Group) on $($install.Server)." -Color "Green"
     }
     }
-    ELSE {
-        Write-Log "Could not find Kaseya installed!" "Red"
+    else {
+        Write-Log "Could not find Kaseya installed!" -Color "Red"
     }
 }
 
-function Get-Trend 
-{
+function Get-Trend {
     $trend = Get-Service -DisplayName "Trend Micro*Security Agent"
-    IF ($trend){
-        Write-Host "Trend Micro : Installed." -ForegroundColor "Green"
-        Switch($trend.Status){
+    if ($trend) {
+        Write-Host "Trend Micro : Installed." -Color "Green"
+        switch($trend.Status) {
             'Running'{Write-Log "$($trend.DisplayName) : $($trend.Status)" "Green"}
             'Stopped'{Write-Host "$($trend.DisplayName) : $($trend.Status)" "Yellow"}
         }
     }
-	ELSE {
-	    Write-Log "Trend Micro : Not installed." "Red"
+	else {
+	    Write-Log "Trend Micro : Not installed." -Color "Red"
     }
 }
 
-function Get-FlashPlayer ($Version)
-{
+function Get-FlashPlayer ($Version) {
     $flashPlayer = Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall,HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall | `
     Get-ItemProperty | Where-Object {$_.DisplayName -like "Adobe Flash Player*$version"}
   
-    IF ($flashPlayer){
-        Write-Log "Adobe Flash Player $version $($flashPlayer.DisplayVersion) : Installed." "Green"
+    if ($flashPlayer) {
+        Write-Log "Adobe Flash Player $version $($flashPlayer.DisplayVersion) : Installed." -Color "Green"
     }
     else {
         Write-Log "Adobe Flash Player $version : Not installed." "Yellow"
     }
 }
 
-function Get-Java
-{
+function Get-Java {
     $java = Get-ChildItem -Path  HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall,HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall | `
     Get-ItemProperty | Where-Object {$_.DisplayName -like "Java ? Update *"}
     
-    IF ($java){
+    if ($java){
         foreach ($javaInstall in $java){
         Write-Log "$($javaInstall.DisplayName) : Installed." "Green"
         } 
     }
-    Else {
+    else {
         Write-Log "Java : Not installed." "Red"
     }
 }
 
-function Get-DnsSettings
-{
+function Get-DnsSettings {
     $dnsSettings = netsh interface ipv4 show dns | Where-Object {![string]::isnullorempty($_)} | ForEach-Object {$_.trim()}
     $hash = @{}
     $ip = New-Object System.Collections.ArrayList
     $results = @(
-        foreach ($line in $dnsSettings){
-            switch -Regex ($line){
+        foreach ($line in $dnsSettings) {
+            switch -Regex ($line) {
                 '^configuration' {
-                    if ($hash.Keys.Count -gt 0){
+                    if ($hash.Keys.Count -gt 0) {
                         New-Object pscustomobject -Property $hash
                         $hash = @{}
                     }
@@ -189,41 +178,39 @@ function Get-DnsSettings
     $wiredDNS = $results | Where-Object {$_.Interface -like "Local*"}
     $wirelessDNS = $results | Where-Object {$_.Interface -like "Wireless*" -or $_.Interface -like "Wi-Fi"}
 
-    IF ($wiredDNS){
-        Switch ($wiredDNS.Configuration){
+    if ($wiredDNS) {
+        switch ($wiredDNS.Configuration) {
             'DHCP' {Write-Log "$($wiredDNS.Interface) DNS : $($wiredDNS.Configuration)." "Green"}
             'Static' {Write-Log "$($wiredDNS.Interface) DNS: $($wiredDNS.Configuration)." "Yellow"}
         }
     }
-    IF ($wirelessDNS){
-        Switch ($wirelessDNS.Configuration){
+    if ($wirelessDNS) {
+        switch ($wirelessDNS.Configuration) {
             'DHCP' {Write-Log "$($wirelessDNS.Interface) DNS : $($wirelessDNS.Configuration)." "Green"}
             'Static' {Write-Log "$($wirelessDNS.Interface) DNS : $($wirelessDNS.Configuration)." "Yellow"}
         }
     }
 }
 
-function Get-Drivers
-{
+function Get-Drivers {
     $drivers = Get-WmiObject Win32_PNPEntity | Where-Object {$_.ConfigManagerErrorCode -ne 0} | Select-Object Name
-    IF ($drivers){
-        foreach ($driver in $drivers){
+    if ($drivers){
+        foreach ($driver in $drivers) {
             Write-Log "$($driver.Name) is reporting a driver error." "Red"} 
     }
-    Else {
+    else {
         Write-Log "No driver errors being reported." "Green"
     }
 }
 
-function Get-DiskInfo
-{
+function Get-DiskInfo {
     $disks = Get-WmiObject -Class Win32_LogicalDisk | Where-Object {$_.DriveType -eq "3"}
-    foreach ($disk in $disks){
+    foreach ($disk in $disks) {
         $freeSpace = $disk.FreeSpace/1GB
         $size = $disk.Size/1GB
         $percentFree = [math]::Round($freeSpace/$size*100)
         $driveLetter = $disk.DeviceID
-        IF ($percentFree -lt 25){
+        if ($percentFree -lt 25){
             Write-Log "$driveLetter $percentFree% available." "Red"
         }
         else {
@@ -232,13 +219,12 @@ function Get-DiskInfo
     }
 }   
 
-function Get-SystemRestore
-{
+function Get-SystemRestore {
     $systemRestore = Get-WmiObject -Namespace 'root\default' -Class SystemRestoreConfig
-    IF ($systemRestore.RPSessionInterval -eq "1"){
+    if ($systemRestore.RPSessionInterval -eq "1") {
         Write-Log "System Restore : Enabled using $($systemRestore.DiskPercent)% disk space." "Green"
     }
-    ELSE {
+    else {
         Write-Log "System Restore : Not enabled." "Red"
     }
 }
